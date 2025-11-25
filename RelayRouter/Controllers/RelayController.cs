37053325f63a947b5971065573d6b31c32fdcf1f
Anders Tornbjerg <anderslykketornbjerg@gmail.com>
2025-11-25 11:06:51 +0100

    Milestone build
    Update docker file with port
    Move controllers to own class and file.
    Clean program.cs

---
+++ RelayRouter/Controllers/RelayController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RelayRouter.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class RelayController : ControllerBase
+    {
+        [HttpGet("health")]
+        public async Task<IActionResult> Health()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost("MakeConnection")]
+        public async Task<IActionResult> FoundMatch([FromBody] string JWT)
+        {
+            return Ok();
+        }
+    }
+}
